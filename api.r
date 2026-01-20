ab78df8e86d000a9bb36fcfd8321263e9c07cec4
hcr64 <149124502+hcr64@users.noreply.github.com>
2026-01-19 18:08:14 -0700

    Update api.r, pull data from NEON

--- api.r
+++ api.r
@@ -1,1 +1,22 @@
-a
+# install needed packages
+install.packages("neonUtilities")
+
+# include downloaded packages
+library(neonUtilities)
+
+# source the token
+source("neon_token_source.R")
+
+# pull some data
+foliar <- loadByProduct(dpID="DP1.10026.001", site="HARV", 
+                        package="expanded", check.size=F,
+                        token=NEON_TOKEN)
+
+# examine the foliar data
+summary(foliar)
+
+# pull a column from cfc_carbonNitrogen 
+carbon_pct <- foliar[["cfc_carbonNitrogen"]][["carbonPercent"]]
+CN_ratio <- foliar[["cfc_carbonNitrogen"]][["CNratio"]]
+
+carbon_pct
